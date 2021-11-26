08ba18897cce23a8792db72efee9fd37d1b70cd3
Jennifer Hong <hongjenn24@gmail.com>
2021-11-25 17:17:37 -0800

    commit tree 8891938c6255d6cc732814be23e0fb40dd8de05f

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Code your solution here
+
+// findMatching()
+
+// first set
+// const findMatching = function(drivers, string) {
+//     return drivers.filter(function(driver) {
+//       return driver.toLowerCase() === string.toLowerCase()
+//     })
+// }
+// practice test to prove that const helloWorld = () => 'Hello World!';
+//  const findMatching = function(drivers, string) {
+//     let correctName = drivers.filter(driver => driver.toLowerCase() === string.toLowerCase())
+//     return correctName
+// }
+ function findMatching(drivers, string) {
+     let correctName = drivers.filter(driver => driver.toLowerCase() === string.toLowerCase())
+     return correctName;
+ }
+
+ // fuzzyMatch()
+
+function fuzzyMatch(drivers, string) {
+    let correctLetters = drivers.filter(beginningLetters => beginningLetters.substring(0,2) === string);
+    return correctLetters;
+}
+
+// matchName()
+
+function matchName(drivers, string) {
+    let returnedName = drivers.filter(listedName => listedName.name === string);
+    return returnedName;
+}
